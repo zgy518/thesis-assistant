2a8f1af8959229578ad58d42fa8d700126883059
zgy518 <18800492787@163.com>
2026-05-24 15:57:33 +0800

    Fix: add basename for GitHub Pages routing

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/thesis-assistant">
       <App />
       <Toaster position="top-right" richColors closeButton />
     </BrowserRouter>
